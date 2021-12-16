2a746af8c50bf97829e4d72baa4b06495e52c532
Fabricio Ceppi <fabricioceppi@gmail.com>
2021-12-16 13:37:24 -0300

    Limpieza App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
     window.addEventListener("resize", () => {
       let vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty("--vh", `${vh}px`);
-      console.log(vh);
     });
   }, []);
 
