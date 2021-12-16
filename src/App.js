38dbda73bc80f48214621d97aba981ddca867cdb
Fabricio Ceppi <fabricioceppi@gmail.com>
2021-12-16 13:31:49 -0300

    Fix, altura dinámica mobile con js (--vh)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "./assets/components/Hero";
 import About from "./assets/components/About";
 import Projects from "./assets/components/Projects";
@@ -7,6 +7,16 @@ import DATA from "./data.js";
 import Separator from "./assets/icons/Separator";
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    window.addEventListener("resize", () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+      console.log(vh);
+    });
+  }, []);
 
   return (
     <>
